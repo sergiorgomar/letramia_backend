94a563a6d36f8c8f7785c76fa14f764286f42ee7
sergiorgomar <gomarsergio34@gmail.com>
2026-08-20 10:14:29 -0600

    🗃️  update migrations

---
+++ migrations/0010_fresh_lily_hollister.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "password_reset_tokens" (
+	"hash" varchar(64) PRIMARY KEY NOT NULL,
+	"user_id" uuid NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"used_at" timestamp
+);
+--> statement-breakpoint
+ALTER TABLE "password_reset_tokens" ADD CONSTRAINT "password_reset_tokens_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
